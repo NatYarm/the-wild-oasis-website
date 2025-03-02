202440308a3e50cbc100ecead130b70566cb846b
Natalia Yarmolinskaya <nat_yarm@natalias-mbp.home>
2025-03-02 22:16:09 +0100

    Optimistic delete reservation

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,7 +1,7 @@
-import ReservationCard from '@/app/_components/ReservationCard';
+import Link from 'next/link';
+import ReservationList from '@/app/_components/ReservationList';
 import { auth } from '@/app/_lib/auth';
 import { getBookings } from '@/app/_lib/data-service';
-import Link from 'next/link';
 
 export const metadata = {
   title: 'Reservations',
@@ -25,11 +25,7 @@ export default async function Page() {
           </Link>
         </p>
       ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
+        <ReservationList bookings={bookings} />
       )}
     </div>
   );
